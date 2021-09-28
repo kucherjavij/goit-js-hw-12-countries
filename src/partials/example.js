b3318de9f2af723176b1cfbd9ad16abeb39c3d78
samuel shuman <81707204+kucherjavij@users.noreply.github.com>
2021-09-29 00:51:17 +0300

    up

--- src/partials/example.js
+++ src/partials/example.js
@@ -3,7 +3,7 @@
 
 
 function getCountries(searchQuery) {
-    return fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(response => response.json())
+    return fetch(`https://restcountries.com/v3/name/${searchQuery}`).then(response => response.json())
 }
 
 export default { getCountries };
